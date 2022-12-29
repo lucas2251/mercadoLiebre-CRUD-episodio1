16b1829c729aa3be4c1c594b0c6d1f68e413f7b0
lucas2251 <lucascordoba2@hotmail.com>
2022-12-29 16:18:51 -0300

    termino tp 12

---
+++ src/controllers/productsController.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+		res.render('products',{
+			products,
+			toThousand
+		})
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		const id = +req.params.id;
+		let product = products.find(product => product.id === id);
+		return res.render('detail' , {
+			product,
+			toThousand
+		})
+
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+		// Do the magic
+		return res.render('product-create-form')
+	},
+	
+	// Create -  Method to store
+	store: (req, res) => {
+		// Do the magic
+		return res.send(req.body)
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		// Do the magic
+		const id = +req.params.id;
+		let product = products.find(product => product.id === id);
+		return res.render('product-edit-form', {
+			productToEdit : product
+		})
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		// Do the magic
+		return res.send('Producto a Actualizar')
+	},
+
+	// Delete - Delete one product from DB
+	destroy : (req, res) => {
+		// Do the magic
+		return res.send('Producto a Eliminar')
+	}
+};
+
+module.exports = controller;
